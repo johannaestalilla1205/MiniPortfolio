2d6a929a5e37fcfa8fd92416b16a50f539d97e7f
Johanna Estalilla <146572225+johannaestalilla1205@users.noreply.github.com>
2026-04-06 19:07:34 +0800

    Added Skills and Achievements

--- MiniPortfolio/Program.cs
+++ MiniPortfolio/Program.cs
@@ -21,6 +21,7 @@ class Portfolio
         Console.WriteLine("PhoneNumber" + PhoneNumber);
 
         Skills();
+        TesdaAchievements();
 
         Console.WriteLine("==============================");
     }
@@ -29,8 +30,18 @@ class Portfolio
     {
         Console.WriteLine("Skills:");
         Console.WriteLine("- SQL Database");
+        Console.WriteLine("- SQL Database");
+        Console.WriteLine("- PHP Programming");
         Console.WriteLine("- Java Programming\n");
     }
+
+    public void TesdaAchievements()
+    {
+        Console.WriteLine("Tesda Achievements:");
+        Console.WriteLine("- TESDA NC II: Java Programming");
+        Console.WriteLine("- TESDA NC II: Computer Servicing  Systems\n");
+    }
+    
 }
 
 class Program
@@ -46,8 +57,9 @@ class Program
         myPortfolio.Email = "pupbsitestalillajohanna@gmail.com";
         myPortfolio.PhoneNumber = "+63 966 395 2110";
 
-
         myPortfolio.DisplayMiniPortfolio();
+
+        Console.WriteLine("==============================");
 
         Console.WriteLine("\nPress any key to exit...");
         Console.ReadKey();
